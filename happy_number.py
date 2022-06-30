3f7c3beea7f679069356eee966edd9929122f30b
21A91A6111 <98648214+21A91A6111@users.noreply.github.com>
2022-06-30 11:22:46 +0530

    n=int(input()) r=b=c=0 while n>0:     r=n%10     b+=pow(r,2)     n=n//10     if(n==0):         if(b>0 and b<=9):             break         else:             n=b             b=0 if(b==1 or b==7):     print(True) else:     print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,20 +1,16 @@
-import math
-def digitsSquareSum(Number):
-    Sum = rem = 0
-    while Number > 0:
-        rem = Number % 10
-        Sum = Sum + math.pow(rem, 2)
-        Number = Number // 10
-    return Sum
-
-
-Number = int(input())
-Temp = Number
-
-while Temp != 1 and Temp != 4:
-    Temp = digitsSquareSum(Temp)
-
-if Temp == 1:
-    print("True")
+n=int(input())
+r=b=c=0
+while n>0:
+    r=n%10
+    b+=pow(r,2)
+    n=n//10
+    if(n==0):
+        if(b>0 and b<=9):
+            break
+        else:
+            n=b
+            b=0
+if(b==1 or b==7):
+    print(True)
 else:
-    print("False")
+    print(False)
