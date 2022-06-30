6e44f9c02a2de39c6f6c59d475b1fe65d3fbcca9
21A91A6111 <98648214+21A91A6111@users.noreply.github.com>
2022-06-30 11:07:38 +0530

    a,b=map(int,input().split()) lcm=1 for i in range(10000):     if lcm%a==0 and lcm%b==0:         break     else:         lcm+=1 print(lcm)

--- LCM.py
+++ LCM.py
@@ -1,10 +1,8 @@
-a,b = map(int,input().split())
-if a>b:
-    max=a
-else:
-    max=b
-while True:
-    if max%a==0 and max%b==0:
-        print(max)
+a,b=map(int,input().split())
+lcm=1
+for i in range(10000):
+    if lcm%a==0 and lcm%b==0:
         break
-    max+=1
+    else:
+        lcm+=1
+print(lcm)
