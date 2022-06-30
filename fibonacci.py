42d7dd56b3dbc38bf179302ebf92ed6bd65d47e3
21A91A6111 <98648214+21A91A6111@users.noreply.github.com>
2022-06-30 11:27:47 +0530

    a=int(input()) fa=0 fb=1 n=0 for i in range(a):     print(fa,end=" ")     n=fa+fb     fa=fb     fb=n

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,9 @@
-n = int(input())
-n1, n2 = 0, 1 
-i = 0 
-if n == 1:
-  print(n)
-  print(n1)
-else:
-  for i in range(n):
-      print(n1,end=' ')
-      sum = n1 + n2
-      n1 = n2
-      n2 = sum
-      i += 1
+a=int(input())
+fa=0
+fb=1
+n=0
+for i in range(a):
+    print(fa,end=" ")
+    n=fa+fb
+    fa=fb
+    fb=n
