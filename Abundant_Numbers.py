94ab0a9b905827817d47baf308fade0724ad7b95
21A91A6111 <98648214+21A91A6111@users.noreply.github.com>
2022-06-30 11:29:49 +0530

    n=int(input()) s=0 for i in range(1,n):     if(n%i==0) :         s=s+i if(s>n) :     print("True") else :     print("False")

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,9 +1,9 @@
 n=int(input())
-f=0
+s=0
 for i in range(1,n):
-    if n%i==0:
-        f+=i
-if f>n:
-    print(True)
-else:
-    print(False)
+    if(n%i==0) :
+        s=s+i
+if(s>n) :
+    print("True")
+else :
+    print("False")
